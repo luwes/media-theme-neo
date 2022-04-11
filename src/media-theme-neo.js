bf3f5c9b12840f05c1a3ca5aeb80e1efc9902574
Wesley Luyten <me@wesleyluyten.com>
2022-04-11 11:48:04 -0500

    Add breakpoints, tweak styles

--- src/media-theme-neo.js
+++ src/media-theme-neo.js
@@ -1,5 +1,5 @@
-import { html, render, unsafeHTML } from '@github/jtml';
 import 'media-chrome';
+import { html, render, unsafeHTML } from '@github/jtml';
 import './media-control-strip.js';
 import './media-playback-rate-range-button.js';
 import './media-playback-rate-range.js';
@@ -17,56 +17,88 @@ import volumeLow from './icons/volume-low.svg';
 import volumeMedium from './icons/volume-medium.svg';
 import volumeHigh from './icons/volume-high.svg';
 
-const template = html`
+const template = (props) => html`
   <style>
     ${cssStr}
   </style>
 
-  <media-controller>
+  <media-controller class="${props.breakpoints}">
     <slot name="media" slot="media"></slot>
 
-    <div class="neo-control-bar">
+    <div slot="centered-chrome" class="xs:hidden">
       <media-play-button>
         ${unsafeHTML(play)} ${unsafeHTML(pause)}
       </media-play-button>
+    </div>
 
-      <media-control-bar>
-        <media-time-display show-duration remaining></media-time-display>
-        <media-time-range></media-time-range>
-        <media-captions-button>
-          ${unsafeHTML(captionsOn)} ${unsafeHTML(captionsOff)}
-        </media-captions-button>
-        <media-control-strip>
-          <media-volume-range></media-volume-range>
-          <media-mute-button slot="open-button">
-            ${unsafeHTML(volumeHigh)} ${unsafeHTML(volumeMedium)}
-            ${unsafeHTML(volumeLow)} ${unsafeHTML(volumeOff)}
-          </media-mute-button>
-        </media-control-strip>
-        <media-control-strip>
-          <media-playback-rate-range></media-playback-rate-range>
-          <media-playback-rate-range-button slot="open-button">
-          </media-playback-rate-range-button>
-        </media-control-strip>
-        <media-pip-button>
-          ${unsafeHTML(pipEnter)} ${unsafeHTML(pipExit)}
-        </media-pip-button>
-        <media-airplay-button></media-airplay-button>
-        <media-fullscreen-button>
-          ${unsafeHTML(fullscreenEnter)} ${unsafeHTML(fullscreenExit)}
-        </media-fullscreen-button>
-      </media-control-bar>
+    <div class="neo-control-bar">
+      <media-play-button class="hidden xs:block">
+        ${unsafeHTML(play)} ${unsafeHTML(pause)}
+      </media-play-button>
+
+      <div class="neo-play-bar">
+        <media-time-range class="mobile sm:hidden xs:rounded-t-sm"></media-time-range>
+        <media-control-bar class="xs:rounded-b-sm sm:rounded-t-sm">
+          <media-time-display show-duration class="hidden xs:inline-flex"></media-time-display>
+          <media-time-range class="desktop hidden sm:block"></media-time-range>
+          <div class="spacer"></div>
+          <media-captions-button>
+            ${unsafeHTML(captionsOn)} ${unsafeHTML(captionsOff)}
+          </media-captions-button>
+          <media-control-strip>
+            <media-volume-range></media-volume-range>
+            <media-mute-button slot="open-button">
+              ${unsafeHTML(volumeHigh)} ${unsafeHTML(volumeMedium)}
+              ${unsafeHTML(volumeLow)} ${unsafeHTML(volumeOff)}
+            </media-mute-button>
+          </media-control-strip>
+          <media-control-strip>
+            <media-playback-rate-range></media-playback-rate-range>
+            <media-playback-rate-range-button slot="open-button">
+            </media-playback-rate-range-button>
+          </media-control-strip>
+          <media-pip-button>
+            ${unsafeHTML(pipEnter)} ${unsafeHTML(pipExit)}
+          </media-pip-button>
+          <media-airplay-button></media-airplay-button>
+          <media-fullscreen-button>
+            ${unsafeHTML(fullscreenEnter)} ${unsafeHTML(fullscreenExit)}
+          </media-fullscreen-button>
+        </media-control-bar>
+      </div>
     </div>
   </media-controller>
 `;
+
+const resizeObserver = new ResizeObserver(function(entries) {
+  entries.forEach(({ target }) => target.render());
+});
 
 class MediaThemeNeo extends HTMLElement {
   constructor() {
     super();
 
     this.attachShadow({ mode: 'open' });
-    render(template, this.shadowRoot);
+    this.render();
+
+    resizeObserver.observe(this);
   }
+
+  render() {
+    render(template({
+      breakpoints: getBreakpoints(this)
+    }), this.shadowRoot);
+  }
+
+  get breakpoints() {
+    return { xs: 396, sm: 484, md: 576, lg: 768, xl: 960 };
+  }
+}
+
+function getBreakpoints(el) {
+  return Object.keys(el.breakpoints).filter(key => {
+    return el.offsetWidth >= el.breakpoints[key];
+  });
 }
 
 if (!customElements.get('media-theme-neo')) {
